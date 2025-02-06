17280032eeb632b194a90e6b700333aad71250a1
az2 <adcahya89@gmail.com>
2025-02-06 11:03:02 +0700

    fix tina build

--- tina/config.ts
+++ tina/config.ts
@@ -18,19 +18,12 @@ export default defineConfig({
   build: {
     outputFolder: "admin",
     publicFolder: "static",
-    basePath: "",
-    host: process.env.NETLIFY ? "https://rafaliserhajaya.com" : "http://localhost:1313",
+    basePath: "admin",
   },
   media: {
     tina: {
       mediaRoot: "static/images",
       publicFolder: "static",
-    },
-  },
-  search: {
-    tina: {
-      indexerToken: process.env.TINA_TOKEN,
-      stopwordLanguages: ['ind']
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
