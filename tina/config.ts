44b1036db7e87cad8c15fc92029a9d042b31d321
az2 <adcahya89@gmail.com>
2025-02-09 17:58:47 +0700

    Update media configuration to use static/images folder

--- tina/config.ts
+++ tina/config.ts
@@ -26,8 +26,9 @@ export default defineConfig({
   
   media: {
     tina: {
-      publicFolder: "public",
-      mediaRoot: "",
+      publicFolder: "static",
+      mediaRoot: "images",
+      static: false,
     },
   },
   search: {
